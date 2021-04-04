7671ae174d680326cfe9d30c5e97d6191beab36f
unizef <zefstribos@gmail.com>
2021-04-04 18:23:32 +0200

    added start method and button

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Reaction Timer Game</h1>
-    <button class="btn" :click="start">Start</button>
+    <button class="btn" @click="start">Start</button>
   </div>
 </template>
 
@@ -11,6 +11,19 @@ import "./assets/styles/global.css";
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      console.log(this.delay);
+    },
+  },
 };
 </script>
 
